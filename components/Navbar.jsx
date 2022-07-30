d0f1a61b4e174c9dfb68b65460a1038f65f773b7
FourSqweez <four7518@gmail.com>
2022-07-30 16:23:48 +0700

    Edit : Extracting classes with @apply to created global bnt-primary class to reuseable

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -84,16 +84,16 @@ const Navbar = () => {
 								let's connect'
 							</p>
 							<div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
-								<div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+								<div className="btn-primary">
 									<FaLinkedinIn />
 								</div>
-								<div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+								<div className="btn-primary">
 									<FaGithub />
 								</div>
-								<div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+								<div className="btn-primary">
 									<AiOutlineMail />
 								</div>
-								<div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+								<div className="btn-primary">
 									<BsFillPersonLinesFill />
 								</div>
 							</div>
