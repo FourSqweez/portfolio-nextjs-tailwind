950a8bd85878d14de7966c9a9618f865edb789fe
FourSqweez <four7518@gmail.com>
2022-07-31 04:22:29 +0700

    Bug fix

--- components/Main.jsx
+++ components/Main.jsx
@@ -7,18 +7,19 @@ const Main = () => {
 			<div className="max-w-5xl w-full h-full mx-auto p-2 flex justify-center items-center">
 				<div>
 					<p className="uppercase text-sm tracking-widest text-gray-600">
-						let's build something together
+						let&apos;s build something together
 					</p>
 					<h1>
 						Hi, I&apos;m <span className="text-[#5651e5]">Four</span>
 					</h1>
 					<h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
 					<p className="py-4 text-gray-600 max-w-[7-0%] m-auto">
-						I&apos;m a front-end web developer specializing in building exceptional digital experiences.
-						Currently, I&apos;m focused on building responsive front-end web
-						applications while learning back-end technologies.
+						I&apos;m a front-end web developer specializing in building
+						exceptional digital experiences. Currently, I&apos;m focused
+						on building responsive front-end web applications while
+						learning back-end technologies.
 					</p>
-					<BtnSocial/>
+					<BtnSocial />
 				</div>
 			</div>
 		</div>
