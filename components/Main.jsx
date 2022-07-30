7c301edcf1d9c55b5057959864949479a59f2d07
FourSqweez <four7518@gmail.com>
2022-07-31 04:07:38 +0700

    Refactor code

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { AiOutlineMail } from 'react-icons/ai'
-import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
-import { BsFillPersonLinesFill } from 'react-icons/bs'
-
-const btnComponents = [
-	{ icon: <FaLinkedinIn /> },
-	{ icon: <FaGithub /> },
-	{ icon: <AiOutlineMail /> },
-	{ icon: <BsFillPersonLinesFill /> },
-]
+import BtnSocial from './BtnSocial'
 
 const Main = () => {
 	return (
-		<div id='home' className="w-full h-screen text-center px-10">
+		<div id="home" className="w-full h-screen text-center px-10">
 			<div className="max-w-5xl w-full h-full mx-auto p-2 flex justify-center items-center">
 				<div>
 					<p className="uppercase text-sm tracking-widest text-gray-600">
@@ -23,19 +14,11 @@ const Main = () => {
 					</h1>
 					<h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
 					<p className="py-4 text-gray-600 max-w-[7-0%] m-auto">
-						I'm a front-end web developer specializing in building (and
-						occasionally designing) exceptional digital experiences.
+						I'm a front-end web developer specializing in building exceptional digital experiences.
 						Currently, I'm focused on building responsive front-end web
 						applications while learning back-end technologies.
 					</p>
-					<div className="flex items-center justify-around max-w-sm m-auto py-4">
-						{btnComponents &&
-							btnComponents.map((btnComponent) => (
-								<div className="btn-primary hover:scale-110 p-6">
-									{btnComponent.icon}
-								</div>
-							))}
-					</div>
+					<BtnSocial/>
 				</div>
 			</div>
 		</div>
