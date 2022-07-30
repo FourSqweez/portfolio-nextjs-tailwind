380238b65d9017c9bb6a06318092b9f3e25645c1
FourSqweez <four7518@gmail.com>
2022-07-31 04:02:39 +0700

    Add : Created reuseable button group

---
+++ components/BtnSocial.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
+import { BsFacebook } from 'react-icons/bs'
+import { SiNotion } from "react-icons/si";
+
+const btnComponents = [
+	{
+		icon: <FaLinkedinIn />,
+		link: 'https://www.linkedin.com/in/jeerasak-krueanaim-5a2289208/',
+	},
+	{ icon: <FaGithub />, link: 'https://github.com/FourSqweez' },
+	{ icon: <AiOutlineMail />, link: 'mailto:kjeerasak@kkumail.com' },
+	{ icon: <BsFacebook />, link: 'https://www.facebook.com/foursqweez' },
+	{icon: <SiNotion/>, link:'https://moored-kettle-0b0.notion.site/Fullstack-Developer-2ed4c3301c734eea96cafca884eab5b1'}
+]
+
+const BtnSocial = () => {
+	return (
+		<div className="flex items-center justify-around max-w-sm m-auto py-4 ">
+			{btnComponents &&
+				btnComponents.map((btnComponent) => (
+					<a href={btnComponent.link} target="_blank" rel="noreferrer">
+						<div className="btn-primary hover:scale-110 p-4 sm:p-6">
+							{btnComponent.icon}
+						</div>
+					</a>
+				))}
+		</div>
+	)
+}
+
+export default BtnSocial
