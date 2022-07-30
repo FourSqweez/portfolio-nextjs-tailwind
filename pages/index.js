149afbcbad6b0a21cf8d90af650af12dd64a013e
FourSqweez <four7518@gmail.com>
2022-07-30 23:01:14 +0700

    Add : Add component to render

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import About from '../components/About'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+import Contact from './../components/Contact'
 
 export default function Home() {
 	return (
@@ -15,6 +18,9 @@ export default function Home() {
 			<Navbar />
 			<Main />
 			<About />
+			<Skills />
+			<Projects />
+			<Contact />
 		</div>
 	)
 }
