42afa524d95fcbfeefc18212b5cccb8262ea66f6
FourSqweez <four7518@gmail.com>
2022-08-03 23:34:20 +0700

    Edit : remove webpack5 config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
 	reactStrictMode: false,
 	swcMinify: true,
-	webpack5: true,
-	webpack: (config) => {
-		config.resolve.fallback = { fs: false }
-
-		return config
-	},
 	images: {
 		domains: ['images.unsplash.com'],
 	},
