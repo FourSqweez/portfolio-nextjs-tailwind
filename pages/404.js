0abc79070841cee860a98f14e9f90ea13ef5ad63
FourSqweez <four7518@gmail.com>
2022-08-04 00:29:43 +0700

    Edit : Add new styles

--- pages/404.js
+++ pages/404.js
@@ -8,7 +8,7 @@ export default function Custom404() {
 	return (
 		<div className="w-full">
 			<Image
-				className="rounded-xl group-hover:opacity-10"
+				className="rounded-xl h-full group-hover:opacity-10"
 				src={Img404}
 				alt="404 page"
 				layout="fill"
@@ -16,18 +16,21 @@ export default function Custom404() {
 				objectPosition="center"
 			/>
 
-			<div className="p-10 items-center flex h-[100vh] relative z-1">
+			<div className="p-10 items-center justify-center lg:justify-start flex h-[100vh] relative z-1">
 				<div className="flex flex-col items-center">
-					<h2 className="text-lg sm:text-4xl text-amber-800">
+					<h2
+						className="text-xl text-green-400 bg-black/50 p-2 rounded-xl sm:text-4xl 
+					lg:bg-white/50 lg:text-amber-800"
+					>
 						Oops!...Page Not Found!
 					</h2>
 					<Link href="/">
 						<button
 							className="
-				btn-primary w-20 sm:w-40 mt-5 from-[#fb923c] to-[#92400e]"
+				btn-primary w-35 sm:w-40 mt-5 from-[#fb923c] to-[#92400e]"
 						>
-							<IoArrowBack className="text-sm sm:text-xl" />
-							<p className="ml-2 underline cursor-pointer sm:text-xl">
+							<IoArrowBack className="text-xl sm:text-xl" />
+							<p className="ml-2 underline cursor-pointer text-xl">
 								Back
 							</p>
 						</button>
