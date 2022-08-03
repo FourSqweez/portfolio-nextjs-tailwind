e5718f571ca9d38e6703ed1eee91acebb85bcc7d
FourSqweez <four7518@gmail.com>
2022-08-04 01:03:07 +0700

    Add : Add bar-of-progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 import Navbar from './../components/Navbar'
 import { useRouter } from 'next/router'
+import Router from 'next/router'
+import ProgressBar from '@badrap/bar-of-progress'
+
+const progress = new ProgressBar({
+	size: 3,
+	color: '#F692B2',
+	className: 'z-50',
+	delay: 100,
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 function MyApp({ Component, pageProps }) {
 	const router = useRouter()
