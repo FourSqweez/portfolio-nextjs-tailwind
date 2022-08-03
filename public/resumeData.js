bc594e924e837c9110ab856ac36aefd19776c999
FourSqweez <four7518@gmail.com>
2022-08-03 22:09:55 +0700

    Edit : Change file name and Edit some of data

---
+++ public/resumeData.js
@@ -0,0 +1,180 @@
+export const data = {
+	main: {
+		name: 'Jeerasak Krueaniam',
+		nickName: 'Four',
+		occupation: 'Front-end Devloper',
+		description: 'I eat, sleep and breathe React',
+		image: 'profilepic.jpg',
+		about: 'bla bla',
+		bio: 'Hi there! I am Four',
+		email: 'kjeerasak@kkumail.com',
+		phone: '+66837543842',
+		address: {
+			street: '123 Dev Lane',
+			city: 'London',
+			state: 'Devosphere',
+			zip: 'W3B D3V',
+		},
+		website: 'https://foursqweez-portfolio.netlify.app/',
+		resumedownload: '',
+		social: [
+			{
+				name: 'facebook',
+				url: 'https://www.facebook.com/foursqweez',
+				className: 'fa fa-facebook',
+				iconName: 'BsFacebook',
+			},
+			{
+				name: 'linkedin',
+				url: 'https://www.linkedin.com/in/jeerasak-krueanaim-5a2289208/',
+				className: 'fa fa-linkedin',
+				iconName: 'FaLinkedinIn',
+			},
+			{
+				name: 'mail',
+				url: 'mailto:kjeerasak@kkumail.com',
+				className: 'fa fa-instagram',
+				iconName: 'AiOutlineMail',
+			},
+			{
+				name: 'github',
+				url: 'https://github.com/FourSqweez',
+				className: 'fa fa-github',
+				iconName: 'FaGithub',
+			},
+			{
+				name: 'notion',
+				url: 'https://github.com/FourSqweez',
+				className: 'fa fa-notion',
+				iconName: 'SiNotion',
+			},
+		],
+	},
+	resume: {
+		skillmessage: 'My Programming Language Proficiency',
+		education: [
+			{
+				school: 'Khon Kean University',
+				degree: "Bachelor's Degree, Computer Science",
+				graduated: 'July 2020',
+				description: '',
+			},
+		],
+		work: [
+			{
+				company: 'Test',
+				title: '',
+				years: 'May 2019 - Present',
+				description: '',
+			},
+		],
+		skills: [
+			{
+				name: 'JavaScript',
+				level: '99%',
+				img: 'assets/skills/aa.png',
+			},
+			{
+				name: 'HTML',
+				level: '95%',
+				img: 'assets/skills/html.png',
+			},
+			{
+				name: 'CSS',
+				level: '95%',
+				img: 'assets/skills/css.png',
+			},
+			{
+				name: 'ReactJs',
+				level: '100%',
+				img: 'assets/skills/react.png',
+			},
+			{
+				name: 'Next JS',
+				level: '85%',
+				img: 'assets/skills/nextjs.png',
+			},
+			{
+				name: 'Node JS',
+				level: '95%',
+				img: 'assets/skills/node.png',
+			},
+			{
+				name: 'Tailwind CSS',
+				level: '95%',
+				img: 'assets/skills/tailwind.png',
+			},
+			{
+				name: 'Firebase',
+				level: '95%',
+				img: 'assets/skills/firebase.png',
+			},
+			{
+				name: 'Git',
+				level: '70%',
+				img: 'assets/skills/github1.png',
+			},
+		],
+	},
+	portfolio: {
+		projects: [
+			{
+				project_id: '1',
+				title: 'Movie Hub',
+				tech: ['React JS', 'Axios', 'Styled-Components', 'Material-UI'],
+				category: '',
+				image: '/images/movies.jpg',
+				urlDemo: 'https://movie-tv-material-ui.vercel.app/',
+				urlGithub: 'https://github.com/FourSqweez/movie-tv-material-ui',
+				url: 'movies',
+			},
+			{
+				project_id: '2',
+				title: 'Tesla Clone',
+				tech: ['React JS', 'Redux', 'Styled-Components'],
+				category: '',
+				image: '/images/tesla.jpg',
+				urlDemo:
+					'https://react-tesla-clone-redux-styled-components.vercel.app/',
+				urlGithub:
+					'https://github.com/FourSqweez/react-tesla-clone-redux-styled-components',
+				url: 'tesla',
+			},
+			{
+				project_id: '4',
+				title: 'Amazon Clone',
+				tech: ['Next JS', 'Redux', 'Tailwind'],
+				category: '',
+				image: '/images/amazon.jpg',
+				urlDemo:
+					'https://nextjs-amazon-clone-tailwind-redux-firebase.vercel.app/',
+				urlGithub:
+					'https://github.com/FourSqweez/nextjs-amazon-clone-tailwind-redux-nextAuth',
+				url: 'amazon',
+			},
+			{
+				project_id: '5',
+				title: 'Air bnb Clone',
+				tech: ['Next JS', 'Tailwind'],
+				category: '',
+				image: '/images/airbnb.jpg',
+				urlDemo: 'https://airbnb-nextjs-tailwind-mapbox.vercel.app/',
+				urlGithub:
+					'https://github.com/FourSqweez/airbnb-nextjs-tailwind-mapbox',
+				url: 'airbnb',
+			},
+		],
+	},
+	testimonials: {
+		testimonials: [
+			{
+				text: '',
+				user: '',
+			},
+			{
+				text: '',
+				user: '',
+			},
+		],
+	},
+}
