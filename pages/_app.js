2b362fd02cf2c80a524f13cbd6c460c9b22632af
FourSqweez <four7518@gmail.com>
2022-08-03 22:12:37 +0700

    Edit : Hide navbar if the rout is equal '/404'

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import Navbar from './../components/Navbar'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter()
 	return (
 		<>
-			<Navbar />
+			{router && router.pathname !== '/404' && <Navbar />}
 			<Component {...pageProps} />
 		</>
 	)
