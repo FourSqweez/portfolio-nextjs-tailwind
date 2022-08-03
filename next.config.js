c382b85aa7daef5a89b1419ccc21aeb1c3b89ce2
FourSqweez <four7518@gmail.com>
2022-08-03 22:14:33 +0700

    Edit : Remove weppack5 config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
 	reactStrictMode: false,
 	swcMinify: true,
-	webpack5: true,
-	webpack: (config) => {
-		config.resolve.fallback = { fs: false }
-
-		return config
-	},
 }
 
 module.exports = nextConfig
